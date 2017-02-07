8b744bd8c493d1ece9d1fec57c780e144642503f
sspatari <spatari.stanislav@gmail.com>
2017-02-08 01:25:19 +0200

    multiple sprites, moving left,right and standing in proper direction

--- src/player.cpp
+++ src/player.cpp
@@ -12,21 +12,28 @@ const float kWalkingAcceleration = 0.0012f; // (pixels / ms) / ms
 const float kMaxSpeedX = 0.325f; // pixels / ms
 }
 
+bool operator<(const Player::SpriteState& a, const Player::SpriteState& b) {
+   if (a.motion_type != b.motion_type) {
+      return a.motion_type < b.motion_type;
+   }
+   if (a.horizontalfacing != b.horizontalfacing) {
+      return a.horizontalfacing < b.horizontalfacing;
+   }
+   return false;
+}
+
 Player::Player(Graphics& graphics, int x, int y) :
    x_(x),
    y_(y),
    velocity_x_(0.0f),
-   acceleration_x_(0.0f)
+   acceleration_x_(0.0f),
+   horizontalfacing_(LEFT)
 {
-   sprite_.reset(new AnimatedSprite(
-         graphics,
-         "content/MyChar.bmp",
-         0, 0, Game::kTileSize, Game::kTileSize,
-         15, 3));
+   initializeSprites(graphics);
 }
 
 void Player::update(int elapsed_time_ms) {
-   sprite_->update(elapsed_time_ms);
+   sprites_[getSpriteState()]->update(elapsed_time_ms);
 
    x_ += round(velocity_x_ * elapsed_time_ms);
    velocity_x_ += acceleration_x_ * elapsed_time_ms;
@@ -40,17 +47,51 @@ void Player::update(int elapsed_time_ms) {
 }
 
 void Player::draw(Graphics& graphics) {
-   sprite_->draw(graphics, x_, y_);
+   sprites_[getSpriteState()]->draw(graphics, x_, y_);
 }
 
 void Player::startMovingLeft() {
    acceleration_x_ = -kWalkingAcceleration;
+   horizontalfacing_ = LEFT;
 }
 
 void Player::startMovingRight() {
    acceleration_x_ = kWalkingAcceleration;
+   horizontalfacing_ = RIGHT;
 }
 
 void Player::stopMoving() {
    acceleration_x_ = 0.0f;
+}
+
+void Player::initializeSprites(Graphics& graphics) {
+   sprites_[SpriteState(STANDING, LEFT)] =
+      boost::shared_ptr<Sprite>(new Sprite(
+         graphics,
+         "content/MyChar.bmp",
+         0, 0, Game::kTileSize, Game::kTileSize));
+   sprites_[SpriteState(WALKING, LEFT)] =
+      boost::shared_ptr<Sprite>(new AnimatedSprite(
+         graphics,
+         "content/MyChar.bmp",
+         0, 0, Game::kTileSize, Game::kTileSize,
+         15, 3));
+   sprites_[SpriteState(STANDING, RIGHT)] =
+      boost::shared_ptr<Sprite>(new Sprite(
+         graphics,
+         "content/MyChar.bmp",
+         0, Game::kTileSize, Game::kTileSize, Game::kTileSize));
+   sprites_[SpriteState(WALKING, RIGHT)] =
+      boost::shared_ptr<Sprite>(new AnimatedSprite(
+         graphics,
+         "content/MyChar.bmp",
+         0, Game::kTileSize, Game::kTileSize, Game::kTileSize,
+         15, 3));
+}
+
+Player::SpriteState Player::getSpriteState() {
+   return SpriteState(
+         acceleration_x_ == 0.0f ? STANDING : WALKING,
+         horizontalfacing_
+         );
 }
